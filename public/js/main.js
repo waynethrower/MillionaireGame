b012bf0d99f20831b324ca890f8972823775a78b
waynethrower <wayne.thrower@hotmail.co.uk>
2021-12-29 18:32:11 +0000

    4 amswer buttons and Question CSS and JS event

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+document.getElementById("answerA").onmousedown = function () {
+    console.log("User moused down A");
+    return true; // Not needed, as long as you don't return false
+};
+
+document.getElementById("answerB").onmousedown = function () {
+    console.log("User moused down B");
+    return true; // Not needed, as long as you don't return false
+};
+
+document.getElementById("answerC").onmousedown = function () {
+    console.log("User moused down C");
+    return true; // Not needed, as long as you don't return false
+};
+
+document.getElementById("answerD").onmousedown = function () {
+    console.log("User moused down D");
+    return true; // Not needed, as long as you don't return false
+};
