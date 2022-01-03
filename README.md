d157da139324eda25bf42363c7a7715af7805bd9
waynethrower <wayne.thrower@hotmail.co.uk>
2022-01-03 18:29:08 +0000

    Questions loop through and alert advises the outcome

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Initializing the Read Me
+Using Fetch get multiple choice questions from https://opentdb.com/api_config.php
+
+Play the Who Wants To Be A Millionaire
--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,13 @@
 //window.addEventListener("load", getUserDetails);
 
 var userName = null;
-var question = "How you doing?"
-var answerA = "Answer A";
-var answerB = "Answer B";
-var answerC = "Answer C";
-var answerD = "Answer D";
-
+var question = "";
+let randomised = [];
+let index = 0;
+let questions;
+let answer;
+let locked = true;
+let score = 0;
 //uncomment when styling of HTML complete
 // function getUserDetails() {
 
@@ -20,50 +21,116 @@ var answerD = "Answer D";
 //     } while (userName = null);
 
        //hide play button
-//     play();
+     play();
 
 // }
 
 //Populate the Question
-function populateQuestion() {
+function populateQuestion(question) {
     qHeader.innerHTML = question;
 }
 
 //Populate the answers
-function populateAnswers() {
+function populateAnswers(randomised) {
     let eleA = document.getElementById("answerA");
-    eleA.innerHTML += ` ${answerA}`;
+    eleA.innerHTML = `A: `;
+    eleA.innerHTML += ` ${randomised[0]}`;
 
     let eleB = document.getElementById("answerB");
-    eleB.innerHTML += ` ${answerB}`;
+    eleB.innerHTML = `B: `;
+    eleB.innerHTML += ` ${randomised[1]}`;
 
     let eleC = document.getElementById("answerC");
-    eleC.innerHTML += ` ${answerC}`;
+    eleC.innerHTML = `C: `;
+    eleC.innerHTML += ` ${randomised[2]}`;
 
     let eleD = document.getElementById("answerD");
-    eleD.innerHTML += ` ${answerD}`;
+    eleD.innerHTML = `D: `;
+    eleD.innerHTML += ` ${randomised[3]}`;
+
+    locked = false;
 }
 
-//Handle answer selection
-document.getElementById("answerA").onmousedown = function () {
-    console.log("User moused down A");
-    return true; // Not needed, as long as you don't return false
-};
+        //Handle answer selection
+        document.getElementById("answerA").onmousedown = function () {
+            if (locked === false) {
+                console.log("User moused down A");
+                locked = true;
+                checkAnswer(0);
+            }
+        };
+        
+        document.getElementById("answerB").onmousedown = function () {
+            if (locked === false) {
+                console.log("User moused down B");
+                locked = true;
+                checkAnswer(1);
+            }
+        };
+        
+        document.getElementById("answerC").onmousedown = function () {
+            if (locked === false) {
+                console.log("User moused down C");
+                locked = true;
+                checkAnswer(2);
+            }
+        };
+        
+        document.getElementById("answerD").onmousedown = function () {
+            if (locked === false) {
+                console.log("User moused down D");
+                locked = true;
+                checkAnswer(3);
+            }
+        };
 
-document.getElementById("answerB").onmousedown = function () {
-    console.log("User moused down B");
-    return true; // Not needed, as long as you don't return false
-};
+//consider passing in the level for deciding on the easy/medium/hard
+async function getQuestions() {
+    let response = await fetch(`https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple`);
+    let json = await response.json();
+    questions = json["results"];
+    console.log(questions);
+    populateQuestion(questions[index].question);
+    answer = questions[index].correct_answer;
+    let wrongAs = questions[index].incorrect_answers;
+    randomised = randomiseAs(answer, wrongAs);
+    populateAnswers(randomised);
 
-document.getElementById("answerC").onmousedown = function () {
-    console.log("User moused down C");
-    return true; // Not needed, as long as you don't return false
-};
+}
 
-document.getElementById("answerD").onmousedown = function () {
-    console.log("User moused down D");
-    return true; // Not needed, as long as you don't return false
-};
+function nextQuestion() {
+    populateQuestion(questions[index].question);
+    answer = questions[index].correct_answer;
+    let wrongAs = questions[index].incorrect_answers;
+    randomised = randomiseAs(answer, wrongAs);
+    populateAnswers(randomised);
+}
+
+function randomiseAs(answer, wrongAs) {
+    let i = Math.floor(Math.random() * 5);
+    wrongAs.splice(i,0, answer);
+    return randomised = [...wrongAs];
+}
+
+function checkAnswer(uAnswerI) {
+   
+    //compare user answer to answer
+    if (answer === randomised[uAnswerI]) {
+        alert("Congratulations, you were correct");
+        index++;
+        score++;
+        nextQuestion();
+        return true;
+    }
+    else {
+        alert("Sorry, that was not the correct answer");
+        index = 0;
+        score = 0;
+        play();
+        return false;
+    }
+
+}
 
 //Run Game
 function play() {
@@ -71,14 +138,20 @@ function play() {
     //set up play game button - hide play button & show game items
     //triggers name input pop up
     //fetch questions - complex mix of subjects easy getting harder
-    //receive questions and answers, randomize A to D, keep track of correct answers
-    populateQuestion();
-    populateAnswers();
 
-    //get answer input
-    //check input is correct
+    //5 Easy
+    //https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple
+    //5 medium
+    //https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple
+    //5 hard
+    //https://opentdb.com/api.php?amount=5&category=9&difficulty=hard&type=multiple
+
+    //receive questions and answers, randomize A to D, keep track of correct answers
+    getQuestions();
+
     //if correct congratulations & next question, update Prize Board
     //else Game Over
 
 }
+
 
